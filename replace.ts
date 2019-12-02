d7fdd6a7bff349e90ebd9593aac1e7b5fbb148e4
Dan Schwartz <schwartz@substantial.com>
2019-12-01 16:46:44 -0800

    Explain parseText

--- replace.ts
+++ replace.ts
@@ -3,6 +3,8 @@ import { getSynonym, WordType } from './thesaurus'
 const letter =
   'You will no doubt be [surprised-adjective] to receive a [letter-noun] from one who is almost a total [stranger-noun] to you, but I hope you will pardon me for my [boldness-noun] when I tell you how truly, how deeply I [love-verb] you.'
 
+// finds markers in the form [word-WordType] and replaces them with a synonym for word.
+// example: [surprised-adjective] might be replaced with "shocked"
 const parseText = async (text: string) => {
   const words = text.split(' ')
   const replaced = await words.map(async word => {
