ff9e272d37b2be63e13dbb97f13b873ecf27c8c6
Mike Judge <mikelovesrobots@gmail.com>
2019-12-06 14:51:29 -0800

    Add the missing lolcat transformations to make the tests pass

--- lolcat/lolcat.ts
+++ lolcat/lolcat.ts
@@ -3,9 +3,27 @@ import { flow } from 'lodash'
 // Speak lolcat: https://speaklolcat.com/
 
 const canHas = text => text.replace(/CAN I HAVE/g, 'I CAN HAS')
+const cheezeburger = text => text.replace(/(A )?CHEESEBURGER/g, 'CHEEZBURGER')
 const dose = text => text.replace(/THOSE/g, 'DOSE')
+const who = text => text.replace(/WHO/g, 'HOO')
+const to = text => text.replace(/TO/g, '2')
+const ur = text => text.replace(/YOUR/g, 'UR')
+const r = text => text.replace(/\bARE/g, 'R')
+const wif = text => text.replace(/WITH/g, 'WIF')
+const wuznot = text => text.replace(/WASN'T/g, 'WUZ NOT')
+const listenin = text => text.replace(/LISTENING/g, 'LISTENIN')
 
 export default (text: string) => {
-  return flow([canHas, dose])(text)
-  // return text
+  return flow([
+    canHas,
+    dose,
+    cheezeburger,
+    who,
+    to,
+    ur,
+    r,
+    wif,
+    wuznot,
+    listenin
+  ])(text)
 }
