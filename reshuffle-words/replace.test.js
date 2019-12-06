a39d9e31450b3997cfab7a436cb4d928003c499b
Cassie Koomjian <cassie@substantial.com>
2019-12-06 14:51:58 -0800

    Shuffle every other word

--- reshuffle-words/replace.test.js
+++ reshuffle-words/replace.test.js
@@ -1,13 +1,12 @@
 import { replace } from './replace'
 import { LETTER } from './text'
-import { reverse } from 'lodash'
 
 jest.mock('lodash', () => ({
   shuffle: thing => thing.reverse()
 }))
 
-test('replace shuffles every third word in the input text', () => {
+test('replace shuffles every other word in the input text', () => {
   expect(replace(LETTER)).toBe(
-    'uoY will no tbuod be surprised ot receive a rettel from one ohw is almost a total stranger ot you'
+    'uoY will on doubt eb surprised ot receive a letter morf one ohw is tsomla a latot stranger ot you'
   )
 })
--- reshuffle-words/replace.ts
+++ reshuffle-words/replace.ts
@@ -2,7 +2,7 @@ import { shuffle } from 'lodash'
 
 export const replace = (text: string) => {
   const transformed = text.split(' ').map((word, index) => {
-    if (index % 3 === 0) {
+    if (index % 2 === 0) {
       return shuffle(word.split('')).join('')
     }
     return word
