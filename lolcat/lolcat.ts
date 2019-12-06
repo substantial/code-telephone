a27ea46a733f96a3f7b9316150d088a4d4d335c3
Dan Schwartz <schwartz@substantial.com>
2019-12-06 14:41:43 -0800

    Start lolcatting

--- lolcat/lolcat.ts
+++ lolcat/lolcat.ts
@@ -1,5 +1,11 @@
+import { flow } from 'lodash'
+
 // Speak lolcat: https://speaklolcat.com/
 
+const canHas = text => text.replace(/CAN I HAVE/g, 'I CAN HAS')
+const dose = text => text.replace(/THOSE/g, 'DOSE')
+
 export default (text: string) => {
-  return text
+  return flow([canHas, dose])(text)
+  // return text
 }
