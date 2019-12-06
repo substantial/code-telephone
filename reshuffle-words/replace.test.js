65f8575ec5eb4c208edc49c00d118e3a39d96878
Mike Judge <mikelovesrobots@gmail.com>
2019-12-06 14:41:04 -0800

    Change reverse to reshuffle

--- reshuffle-words/replace.test.js
+++ reshuffle-words/replace.test.js
@@ -1,7 +1,12 @@
 import { replace } from './replace'
 import { LETTER } from './text'
+import { reverse } from 'lodash'
 
-test('replace reverses every third word in the input text', () => {
+jest.mock('lodash', () => ({
+  shuffle: thing => thing.reverse()
+}))
+
+test('replace shuffles every third word in the input text', () => {
   expect(replace(LETTER)).toBe(
     'uoY will no tbuod be surprised ot receive a rettel from one ohw is almost a total stranger ot you'
   )
--- reshuffle-words/replace.ts
+++ reshuffle-words/replace.ts
@@ -1,10 +1,9 @@
+import { shuffle } from 'lodash'
+
 export const replace = (text: string) => {
   const transformed = text.split(' ').map((word, index) => {
     if (index % 3 === 0) {
-      return word
-        .split('')
-        .reverse()
-        .join('')
+      return shuffle(word.split('')).join('')
     }
     return word
   })
