1d954471867c75d7f62c40bea4ddf6978223d07b
Cassie Koomjian <cassie@substantial.com>
2019-12-06 14:40:20 -0800

    Only replace nouns that definitely nouns

--- noun-highlighter/replace.ts
+++ noun-highlighter/replace.ts
@@ -5,8 +5,9 @@ export const replace = async (text: string) => {
 
   return new Promise<string>((resolve, reject) => {
     let result: string = text
-    wordpos.getNouns(text, nouns => {
-      for (const noun of nouns) {
+    wordpos.getPOS(text, results => {
+      console.log(results)
+      for (const noun of results.nouns) {
         result = result.replace(noun, `**${noun}**`)
       }
       resolve(result)
