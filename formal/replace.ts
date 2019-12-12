22434cbbbb78ec1b6ccfdbd4745830aea7ce7342
John Jensen <john.jensen@substantial.com>
2019-12-12 15:59:09 -0800

    first pass on formal letter

---
+++ formal/replace.ts
@@ -0,0 +1,3 @@
+export const replace = (text: string) => {
+  return text
+}
---
+++ formal/text.ts
@@ -0,0 +1,2 @@
+export const LETTER =
+  'You will no doubt be surprised to receive a letter from one who is almost a total stranger to you.'
