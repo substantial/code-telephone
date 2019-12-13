0d1af45675d684fe4c69666abb26fae93a389ae9
Kelly Towle <kelly.towle@substantial.com>
2019-12-12 16:24:34 -0800

    Test Stuff

--- robot/robot.ts
+++ robot/robot.ts
@@ -1,3 +1,13 @@
 import { flow } from 'lodash'
+import { SLITHERY_SNAKES, HISS, SECRETS } from './text'
 
 // do the thing
+export default (str) => {
+  return str
+    .replace('ES', '3Z')
+    .replace(/S/gi, '5')
+    .replace('E', '3')
+    .replace('I', '1')
+    .replace('SERI0USLY', 'CHEEZEBURGER')
+    .replace()
+}
