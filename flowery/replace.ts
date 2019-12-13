0d89cf84e17d1e7c0fbd326b89f44d0dcdc34723
David Sklenar <david.sklenar@substantial.com>
2019-12-12 16:24:57 -0800

    Try and fail to fix this code

--- flowery/replace.ts
+++ flowery/replace.ts
@@ -1,3 +1,3 @@
 export const replace = (text: string) => {
-  return text
+  return text.replace(/one/i, 'one such as yourself').replace(/you/gi, match => `${match}, respectfully,`).replace(/surprised/, 'surprised, for one such as yourself,')
 }
