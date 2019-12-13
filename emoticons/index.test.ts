699a7a6927138db97880ee27a2e40f80bd426081
Matt Findley <findley@substantial.com>
2019-12-12 16:05:25 -0800

    Initial commit

---
+++ emoticons/index.test.ts
@@ -0,0 +1,16 @@
+const converter = (text: string): string => {}
+
+it('converts any words that are text emoticons into text emoticon', () => {
+  expect(converter('surprised')).toEqual(':0')
+  expect(converter('smug')).toEqual(':>')
+  expect(converter('shrug')).toEqual('¯_(ツ)_/¯')
+  expect(converter('laughing')).toEqual('XD')
+  expect(converter('laugh')).toEqual('XD')
+  expect(converter('laughing out loud')).toEqual('=D')
+  expect(converter('lol')).toEqual('=D')
+  expect(converter('I was surpised to see you laughing out loud!!')).toEqual(
+    'I was :0 to see you =D!!'
+  )
+  expect(converter('')).toEqual('')
+  expect(converter('huh?')).toEqual('huh?')
+})
