0ac48cca5b9df264386b4fdde55eb043a293cc7d
Matt Findley <findley@substantial.com>
2019-12-12 16:25:00 -0800

    WIP

--- new-yorker/new-yorker.ts
+++ new-yorker/new-yorker.ts
@@ -1,1 +1,19 @@
-export default (text: string) => text
+const translations = {
+  ere: 'eah',
+  ought: 'awwt',
+  er: 'ah'
+}
+
+export default (text: string) =>
+  text
+    .split(' ')
+    .map(word => {
+      let value = word
+      Object.keys(translations).forEach(part => {
+        if (word.includes(part)) {
+          value = word.replace(part, translations[part])
+        }
+      })
+      return value
+    })
+    .join(' ')
