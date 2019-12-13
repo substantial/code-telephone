531b5db86a9346022460c55e8f3e2f54796cf3e8
agbales <andrew.bales@substantial.com>
2019-12-12 16:28:12 -0800

    Almost passing :)

--- metal/metal.ts
+++ metal/metal.ts
@@ -1,1 +1,20 @@
-export default (text: string) => text
+var WordPOS = require('wordpos')
+var pluralize = require('pluralize')
+const wordpos = new WordPOS()
+
+export default (text: string) => {
+  let updated = text
+    .replace('u', 'ü')
+    .replace('o', 'ø')
+    .replace('hack', 'hAC/DCk')
+  updated += '⚡️🤘'
+
+  wordpos.getNouns(updated, nouns => {
+    nouns.forEach(noun => {
+      const plural = pluralize(noun, 11, true)
+      updated = updated.replace(noun, plural)
+    })
+  })
+
+  return updated
+}
