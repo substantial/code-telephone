6f4f6bb50ea2c909691779f511e3ca5bb4cad497
Matt Findley <findley@substantial.com>
2019-12-12 16:48:36 -0800

    Add more tests

--- delemma/delemma.test.ts
+++ delemma/delemma.test.ts
@@ -8,6 +8,18 @@ describe('delemma', () => {
     expect(delemma("Let's cooperate")).toBe("Let's coöperate")
   })
 
+  it('changes laocoon to laocoön', () => {
+    expect(delemma('laocoon, the son of Acoetes')).toBe(
+      'Laocoön, the son of Acoetes'
+    )
+  })
+
+  it('changes upcase versions of the word', () => {
+    expect(delemma('Laocoon, the son of Acoetes')).toBe(
+      'Laocoön, the son of Acoetes'
+    )
+  })
+
   it('changes reelect to reëlect', () => {
     expect(delemma("Let's reelect our congresswoman")).toBe(
       "Let's reëlect our congresswoman"
@@ -16,5 +28,12 @@ describe('delemma', () => {
 
   it('changes naive to naïve', () => {
     expect(delemma("Don't be naive")).toBe("Don't be naïve")
+  })
+
+  it('handles punctuation', () => {
+    expect(delemma("Don't be naive!")).toBe("Don't be naïve!")
+    expect(delemma("Don't be naive, just do it!")).toBe(
+      "Don't be naïve, just do it!"
+    )
   })
 })
