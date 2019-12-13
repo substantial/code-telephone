bc7cbfeaf1987b13fdef967a2b5c7ef53c75d834
evelyn <evelyn@substantial.com>
2019-12-12 16:38:10 -0800

    add magic values array

--- emoticons/index.ts
+++ emoticons/index.ts
@@ -7,12 +7,22 @@ const replacements = {
   laugh: 'XD',
   lol: '=D'
 }
-
+const magicValues = [
+  '(⊃｡•́‿•̀｡)⊃━✿✿✿✿✿✿',
+  '(∩ᄑ_ᄑ)⊃━☆ﾟ*･｡*･:≡( ε:)',
+  "(/￣ー￣)/~~☆'.･.･:★'.･.･:☆",
+  '(⊃｡•́‿•̀｡)⊃━✿✿✿✿✿✿'
+]
 const converter = (text: string): string => {
+  if (text === 'magic') {
+    magicValues.forEach(val => {
+      console.log({ val })
+      return val
+    })
+  }
   Object.entries(replacements).forEach(([key, value]) => {
     text = text.replace(key, value)
   })
   return text
 }
-
 export default converter
